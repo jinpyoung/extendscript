3927a0b7a99dfa828ab3731b76c938ce8e9bc1a9
jinpyoung <jinpyoung@treenod.com>
2022-01-29 16:42:14 +0900

    스토리 이미지 적용 jsx

--- StoryScrapMaker.jsx
+++ StoryScrapMaker.jsx
@@ -1,105 +1,34 @@
 var root = "/Users/woody/Desktop/StoryScrapMaker/"
-var csvFile = root + "master_StoryScrap.csv"    // 데이터 시트
-var saveDir = root + "Final/"
 var srcDir = root + "Src/"
-var origin_L = root + "origin_StoryImage_L.psd"
-var origin_S = root + "origin_StoryImage_S.psd"
+var srcFiles = []
 var cnt = 0
-var srcPath = ""
-var dataArr = []
 
-main()
+// 소스 폴더에서 파일 확인하기
+var srcFolder = new Folder(srcDir)
+srcFiles = srcFolder.getFiles()
 
-function main() {
+var doc = app.activeDocument
+var pngSaveName = doc.fullName.toString().replace(".psd","")    // png 저장 파일명
+var layers = doc.layers
+var subGroups = layers[0].layerSets
 
-    getDataFromMaster( csvFile )  // 마스터데이터 추출
-    openFile(origin_L)  // 스크랩 이미지용 PSD 열기
-
-    var doc = app.activeDocument,
-        layersRef = doc.layers
-
-    for (var x = 0; x < layersRef.length; x++) {
-
-        doc.activeLayer = doc.layers[x]
+for (k = 0; subGroups.length > k; k++) {
+    var sublayers = subGroups[k].layers
+    for (i = 0; sublayers.length > i; i++) {
+        doc.activeLayer = sublayers[i]
         var ref = new ActionReference()
-            ref.putEnumerated(charIDToTypeID("Lyr "),
-            charIDToTypeID("Ordn"),
-            charIDToTypeID("Trgt"))
+        ref.putEnumerated(charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"))
         var desc = executeActionGet(ref)
+
         if (desc.hasKey(charIDToTypeID('Grup')) && desc.getBoolean(charIDToTypeID('Grup'))) {
-
-            if (cnt == 0) {
-                srcPath = root + dataArr[1][0] + ".png"
-                RelinkToFile(srcPath)
-                cnt++
-            } else if (cnt == 1) {
-                srcPath = root + dataArr[2][0] + ".png"
-                RelinkToFile(srcPath)
-                cnt++
-            } else if (cnt == 2) {
-                srcPath = root + dataArr[3][0] + ".png"
-                RelinkToFile(srcPath)
-                cnt++
-            } else if (cnt == 4) {
-                srcPath = root + dataArr[4][0] + ".png"
-                RelinkToFile(srcPath)
-                cnt++
-            } else if (cnt == 3) {
-                srcPath = root + dataArr[5][0] + ".png"
-                RelinkToFile(srcPath)
-                cnt++
-            } else if (cnt == 5) {
-                srcPath = root + dataArr[6][0] + ".png"
-                RelinkToFile(srcPath)
-                cnt++
-            }
+            RelinkToFile(srcFiles[cnt])
+            cnt++
         }
     }
-    alert("스토리 카드 제작이 완료되었습니다.")
 }
 
-// 모든 그룹을 찾아서 배열로 반환
-// return type : Array
-function getLayerSetsArray() {
-
-    var doc = app.activeDocument
-    var groups = []
-
-    // 레이어중 그룹들을 찾아서 groups 에 추가한다.
-    for(var z=0; z < doc.layers.length; z++){
-        if(doc.layers.layerSets) {
-            groups.push(doc.layers)
-        }
-    }
-
-    // 모든 서브 그룹을 찾아서 groups 에 추가한다.
-    for(var z=0; z<groups.length; z++) { // groups 내부를 탐색
-        for(var e=0; e<groups.layers.length; e++) {
-            if(groups.layers.layerSets) {
-                groups.push(groups.layers)
-            }
-        }
-    }
-
-    return groups
-}
-
-// 작업용 PSD 파일 오픈
-function openFile(fileObj) {
-    try{ var flag = app.open(File(fileObj)) } 
-    catch(e) { alert(e) }
-
-    if(flag) return true
-}
-
-// name으로 레이어 선택(레이어만 선택됨)
-function selectLayerByName(name) {
-    var desc = new ActionDescriptor();
-    var ref = new ActionReference();
-    ref.putName( charIDToTypeID( "Lyr " ), name );
-    desc.putReference( charIDToTypeID( "null" ), ref );
-    executeAction( charIDToTypeID( 'slct' ), desc, DialogModes.NO );
-}
+purposeSave(pngSaveName + ".", "png24")
+alert("모든 이미지가 교체되었습니다.")
 
 // 이미지파일 링크 대체
 function RelinkToFile (fileObj) {
@@ -112,22 +41,60 @@ function RelinkToFile (fileObj) {
    executeAction( idplacedLayerRelinkToFile, desc4, DialogModes.NO );
 }
 
-// 마스터 데이터 추출하기
-function getDataFromMaster( masterfile ) {
+// 파일 저장 옵션
+function purposeSave(fileObj, extention) {
     
-    var row = [], col = []
-    
-    try {
-        var file = new File( masterfile )
-        file.open("r")
-        var txt = file.read()
-        row = txt.toString().split("\n")
+    purposeSave["jpg"] = [];
+    purposeSave["psd"] = [];
+    purposeSave["png8"] = [];
+    purposeSave["png24"] = [];
+
+    //jpg 로 저장
+    purposeSave["jpg"] = function export_jpg(jpgQuary) {
+        fileObj = new File(fileObj+".jpg");
+        var exp = new ExportOptionsSaveForWeb();
+        exp.format = SaveDocumentType.JPEG;
+        exp.interlaced　= false;
+        exp.optimized= true;
+        exp.quality = jpgQuary;
+        activeDocument.exportDocument(fileObj, ExportType.SAVEFORWEB, exp);
+    }
+
+    // psd 로 저장
+    purposeSave["psd"] = function export_psd() {
+        fileObj = new File(fileObj);
+        psdOpt = new PhotoshopSaveOptions();
+        psdOpt.alphaChannels = true;
+        psdOpt.annotations = true;
+        psdOpt.embedColorProfile = false;
+        psdOpt.layers = true;
+        psdOpt.spotColors = false;
+        activeDocument.saveAs(fileObj, psdOpt, true, Extension.LOWERCASE);
+    }
+
+    // png24 로 저장
+    purposeSave["png24"] = function export_png24() {
+        fileObj = new File(fileObj);
+        pngOpt = new PNGSaveOptions();
+        pngOpt.PNG8 = false;
+        pngOpt.interlaced = false;
+        activeDocument.saveAs(fileObj, pngOpt, true, Extension.LOWERCASE);
+    }
+
+    // png 8로 저장
+    purposeSave["png8"] = function export_png8() {
+        fileObj = new File(fileObj+".png");
+        // 포토샵의 png8 저장 옵션 설정
+        var pngOpt  = new ExportOptionsSaveForWeb();
+        pngOpt.format = SaveDocumentType.PNG;
+        pngOpt.PNG8 = true;
+        pngOpt.interlaced = false;
+        pngOpt.compression = 9;     //圧縮率の設定
+        activeDocument.exportDocument(fileObj,ExportType.SAVEFORWEB, pngOpt);
+    }
         
-        for(var i = 0; row.length > i; i++) {
-            col = row[i].toString().split(",")
-            dataArr.push(col)
-        }
-        return dataArr
-        
-    } catch(e) { alert("마스터 파일을 찾지 못했습니다.") }
+    if(extention == "psd"){purposeSave["psd"]();}
+    if(extention == "png8"){purposeSave["png8"]();}
+    if(extention == "png24"){purposeSave["png24"]();}
+    if(extention == "jpg"){purposeSave["jpg"](80);}//jpgのクオリティ設定
 }
